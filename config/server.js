6117dea2b2b8ac4e429a36a7dbdd28eecc33b494
Jackson Dantas <jackson.ap.dantas@hotmail.com>
2018-05-29 15:42:06 -0300

    refatorando rotas com consign

--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 var express = require('express'); // require importa algo para o seu script (nesse caso a biblioteca do express)
+var consign = require('consign'); //importando o consign
+
 var app = express(); //express apenas retorna a função, então é necessário ter o retorno dela
 app.set('view engine', 'ejs');
 app.set('views','./app/views');
+
+consign().include('/app/routes').into(app);
 
 module.exports = app;
