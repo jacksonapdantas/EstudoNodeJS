edd0ce420a5da3e01f58a7233cb4725dbe3c9352
Jackson Dantas <jackson.ap.dantas@hotmail.com>
2018-06-11 17:16:53 -0300

    recebendo dados de formulario via POST com body-parse

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 var express = require('express'); // require importa algo para o seu script (nesse caso a biblioteca do express)
 var consign = require('consign'); //importando o consign
+var bodyParser = require('body-Parser');
 
 var app = express(); //express apenas retorna a função, então é necessário ter o retorno dela
 app.set('view engine', 'ejs');
 app.set('views','./app/views');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 consign()
 	.include('/app/routes')
