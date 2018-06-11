cb869335ee3f4fd29f98d5b5fbffb29349e6fa25
Jackson Dantas <jackson.ap.dantas@hotmail.com>
2018-06-11 17:36:00 -0300

    recebendo dado via POST 2

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -5,6 +5,10 @@ module.exports = function(){
 		connection.query('select * from noticias', callback);
 	}
 
+	this.getNoticia =  function(connection, callback){
+		connection.query('select * from noticias where id_noticia = 2', callback);
+	}
+
 	return this;
 
 };
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,13 @@ module.exports = function(app){
 	app.post('/noticias/salvar', function(req, res){
 		var  noticias = req.body;
 		res.send(noticias);
+
+		var connection = app.config.dbConnection();
+		var noticiasModel = app.app.models.noticiasModel;
+
+		noticiasModel.getNoticias(connection, function(erro, result){
+			res.render("noticias/noticias", {noticias : result});
+		})
 	});
 
 
